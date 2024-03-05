d542d08012b911e790f1c502963286f3f27bfe21
Tomas Roa <tomas.roa@gmail.com>
2024-03-05 19:45:32 -0300

    se sube desasfio udo de variables y operadores aritmeticos

---
+++ assets/index.js
@@ -0,0 +1,48 @@
+// Programa de ecuaciones aritmeticas con dos numeros
+
+let num1 = prompt("Ingresar un numero mayor a cero");
+let num2 = prompt("Ingresar un numero distinto al anterior mayor a cero");
+
+let suma = parseInt(num1) + parseInt(num2);
+let resta = parseInt(num1) - parseInt(num2);
+let division = parseInt(num1) / parseInt(num2);
+let multiplicacion = parseInt(num1) * parseInt(num2);
+let modulolo = parseInt(num1) % parseInt(num2);
+
+document.write(`El resultado de la suma es ${suma}, `);
+document.write(`el resultado de la resta es ${resta}, `);
+document.write(`el resultado de la division es ${division}, `);
+document.write(`el resultado de la multiplicacion es ${multiplicacion}, `);
+document.write(`el resultado del modulo es ${modulolo} . `);
+
+
+
+
+// Programa de transformacion de grados Celcius en Farenheit y Kelvin.
+
+// let Ndias = parseInt(prompt("Ingrese numero de dias a calcular"));
+
+// const ano = 365;
+// const semana = 7;
+// const dia = 1;
+
+// let diasAno = Math.floor(Ndias / ano);
+// let semAno = Math.floor((Ndias % ano)/7);
+// let diaAno = Math.floor((Ndias % ano)%7);
+
+// document.write(`${Ndias} es equivalente a ${diasAno} años, ${semAno} semanas y ${diaAno} dias. `)
+
+
+
+// Programa que solicita numeros
+
+// let num1 = parseInt(prompt("Ingresar un numero mayor a cero"));
+// let num2 = parseInt(prompt("Ingresar un numero mayor a cero"));
+// let num3 = parseInt(prompt("Ingresar un numero mayor a cero"));
+// let num4 = parseInt(prompt("Ingresar un numero mayor a cero"));
+// let num5 = parseInt(prompt("Ingresar un numero mayor a cero"));
+
+// let suma = num1 + num2 + num3 + num4 + num5;
+// let prom = (suma/5);
+
+// document.write(`La suma de los numeros ingresados es: ${suma} y el promedio de esos numeros es ${prom}.`);
