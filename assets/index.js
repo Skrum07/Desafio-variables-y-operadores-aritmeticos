2dd7305196d5ab57b77b4c5d2e3cbe33247cb17a
Tomas Roa <tomas.roa@gmail.com>
2024-03-05 19:53:52 -0300

    se corrige error en el ejercicio de la transformacion de grados

--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,34 @@
 // Programa de ecuaciones aritmeticas con dos numeros
 
-let num1 = prompt("Ingresar un numero mayor a cero");
-let num2 = prompt("Ingresar un numero distinto al anterior mayor a cero");
+// let num1 = prompt("Ingresar un numero mayor a cero");
+// let num2 = prompt("Ingresar un numero distinto al anterior mayor a cero");
 
-let suma = parseInt(num1) + parseInt(num2);
-let resta = parseInt(num1) - parseInt(num2);
-let division = parseInt(num1) / parseInt(num2);
-let multiplicacion = parseInt(num1) * parseInt(num2);
-let modulolo = parseInt(num1) % parseInt(num2);
+// let suma = parseInt(num1) + parseInt(num2);
+// let resta = parseInt(num1) - parseInt(num2);
+// let division = parseInt(num1) / parseInt(num2);
+// let multiplicacion = parseInt(num1) * parseInt(num2);
+// let modulolo = parseInt(num1) % parseInt(num2);
 
-document.write(`El resultado de la suma es ${suma}, `);
-document.write(`el resultado de la resta es ${resta}, `);
-document.write(`el resultado de la division es ${division}, `);
-document.write(`el resultado de la multiplicacion es ${multiplicacion}, `);
-document.write(`el resultado del modulo es ${modulolo} . `);
+// document.write(`El resultado de la suma es ${suma}, `);
+// document.write(`el resultado de la resta es ${resta}, `);
+// document.write(`el resultado de la division es ${division}, `);
+// document.write(`el resultado de la multiplicacion es ${multiplicacion}, `);
+// document.write(`el resultado del modulo es ${modulolo} . `);
 
 
 
 
 // Programa de transformacion de grados Celcius en Farenheit y Kelvin.
+
+// let tempCelc = prompt("Ingrese temperatura en ˚C");
+
+// const tempKelv = parseFloat(tempCelc + 273.15);
+// const tempFar = parseFloat(tempCelc * (9/5) + 32);
+
+// document.write(`${tempCelc}˚C equivalen a ${tempKelv} ˚K y a ${tempFar} ˚F .`);
+
+
+// Programa que transforma dias en su equivalencia en años.
 
 // let Ndias = parseInt(prompt("Ingrese numero de dias a calcular"));
 
@@ -32,17 +42,15 @@ document.write(`el resultado del modulo es ${modulolo} . `);
 
 // document.write(`${Ndias} es equivalente a ${diasAno} años, ${semAno} semanas y ${diaAno} dias. `)
 
-
-
 // Programa que solicita numeros
 
-// let num1 = parseInt(prompt("Ingresar un numero mayor a cero"));
-// let num2 = parseInt(prompt("Ingresar un numero mayor a cero"));
-// let num3 = parseInt(prompt("Ingresar un numero mayor a cero"));
-// let num4 = parseInt(prompt("Ingresar un numero mayor a cero"));
-// let num5 = parseInt(prompt("Ingresar un numero mayor a cero"));
+let num1 = parseInt(prompt("Ingresar un numero mayor a cero"));
+let num2 = parseInt(prompt("Ingresar un numero mayor a cero"));
+let num3 = parseInt(prompt("Ingresar un numero mayor a cero"));
+let num4 = parseInt(prompt("Ingresar un numero mayor a cero"));
+let num5 = parseInt(prompt("Ingresar un numero mayor a cero"));
 
-// let suma = num1 + num2 + num3 + num4 + num5;
-// let prom = (suma/5);
+let suma = num1 + num2 + num3 + num4 + num5;
+let prom = (suma/suma.length);
 
-// document.write(`La suma de los numeros ingresados es: ${suma} y el promedio de esos numeros es ${prom}.`);
+document.write(`La suma de los numeros ingresados es: ${suma} y el promedio de esos numeros es ${prom}.`);
